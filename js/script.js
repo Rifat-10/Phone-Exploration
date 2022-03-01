f7f4a8896a6f17168509b704fef9a9110b85938c
Rifat-10 <mehedihassanrifat.3@gmail.com>
2022-03-01 21:56:31 +0600

    First-Part

--- js/script.js
+++ js/script.js
@@ -13,3 +13,13 @@ const dataLoadingNone =() => {
     const getData = document.getElementById("data")
     getData.style.display = "block";
 }
+const allPhone =() => {
+    const searchValue = document.getElementById("search-input").value;
+    const url = 'https://openapi.programming-hero.com/api/phones?search=${searchText}'
+    console.log(url);
+    fetch(url)
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+
+    console.log(searchValue);
+}
