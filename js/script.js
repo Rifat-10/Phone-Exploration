e090d845a4dc7c5fc29e3526bdb0447a8f564475
Rifat-10 <mehedihassanrifat.3@gmail.com>
2022-03-02 00:13:22 +0600

    Show all phone

--- js/script.js
+++ js/script.js
@@ -16,13 +16,29 @@ const dataLoadingNone =() => {
 const allPhone =() => {
     const searchText = document.getElementById("search-input").value;
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
-    console.log(url);
     fetch(url)
     .then(response => response.json())
     .then(data =>showPhoneDetails(data.data));
 };
 const showPhoneDetails = (data) =>{
+  for(const phone of data){
+    const parent=document.getElementById("phone-container");
+
+    const div= document.createElement("div");
+    div.innerHTML =`<div class="card border p-5">
+    <div class="pro-pic ">
+        <img class="w-25" src="${phone.image}" alt="">
+    </div>
+    <h2>Name: ${phone.phone_name} </h2>
+    <h2>Brand Name: ${phone.brand} </h2>
+    <div class="button2">
+        <button onclick="details()" class="btn-success details">Details</button>
+    </div>
+</div>`;
+parent.appendChild(div);
+  }
+};
+
+const details =() =>{
     
-    console.log(data);
-   
 }
