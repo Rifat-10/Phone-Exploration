ba3b5cd91d5f2a99fb49183d05576f43f856db87
Rifat-10 <mehedihassanrifat.3@gmail.com>
2022-03-02 18:20:43 +0600

    Last- Part

--- js/script.js
+++ js/script.js
@@ -1,13 +1,37 @@
-
 const allPhone = () => {
     const searchText = document.getElementById("search-input").value;
+    const parent = document.getElementById("phone-container");
+    parent.innerHTML = ""
+    const parentnf = document.getElementById("not-found-container");
+    parentnf.innerHTML = ""
+    const parentdt = document.getElementById("details-container");
+    parentdt.innerHTML = ""
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
     fetch(url)
         .then(response => response.json())
         .then(data => showPhone(data.data));
+    const loadingBar = document.getElementById("loading")
+    loadingBar.style.display = "block"
 };
+
 const showPhone = (data) => {
-    for (const phone of data) {
+    const loadingBar = document.getElementById("loading")
+    loadingBar.style.display = "none"
+
+    if (data.length === 0) {
+        const parent = document.getElementById("not-found-container");
+        const div = document.createElement("div");
+        div.innerHTML = `
+        <div class="bg-secondary card border p-5 shadow-lg p-3 mb-3 bg-body rounded">
+            <p class="text-danger"><b>Your phone is not found</b></p>
+        </div>
+        `;
+        parent.appendChild(div);
+    }
+
+    const limitedPhones = data.slice(0, 20);
+    for (const phone of limitedPhones) {
+
         const parent = document.getElementById("phone-container");
 
         const div = document.createElement("div");
@@ -19,7 +43,7 @@ const showPhone = (data) => {
                      <h3><b> ${phone.phone_name} </b></h2>
                      <p>Brand Name: ${phone.brand} </p>
             <div class="button2">
-                <button class="btn-success details" onclick="details('${phone.slug}')" >Details</button>
+                <button class="btn-success details w-25 rounded-pill" onclick="details('${phone.slug}')" >Details</button>
             </div>
 
         </div>`;
@@ -37,11 +61,11 @@ const details = (id) => {
 
 const showPhoneDetails = (info) => {
     document.getElementById("details-container").innerHTML = `
-    <div class="card border p-5 w-auto border border-2">
-        <div class="pro-pic ">
-                <img class="w-25 pb-5" src="  ${info.image}" alt="">
+    <div class="container bg-white border p-3 p-md-5 mx-auto border border-2">
+        <div class="pro-pic w-100 d-flex justify-content-center align-items-center">
+            <img class="pb-5 mx-auto" width="50%" src="${info.image}" alt="">
         </div>
-        <div class="">
+        <div class="container text-break d-flex flex-column fs-6">
             <h1 class="pb-3">  ${info.name} </h1>
             <p><b>Release Date:</b> ${info.releaseDate ? info.releaseDate : "No release date found"}</p>
             <p><b>Id:</b> ${info.slug} </p>
